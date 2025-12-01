7ae12a335f6fb287d238e083b4341d97ddba5e93
jiesir <jiesir16@gmail.com>
2025-12-01 19:11:34 +0800

    Add test dependency and basic application test for service-order module

---
+++ services/service-order/src/test/java/jiesir/z/OrderApplicationTest.java
@@ -0,0 +1,16 @@
+package jiesir.z;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OrderApplicationTest {
+
+
+    @Test
+    void test() {
+        System.out.println("test");
+    }
+
+}
